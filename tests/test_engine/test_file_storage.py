591e8e82ea4c6cc1e81e92cac06109ab9bba3134
abel-alx-github <abeltibeso323@gmail.com>
2024-01-14 03:42:04 -0800

    the Airbnb & consol

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -16,26 +16,33 @@ import unittest
 
 class Test_FileStorage_cls_(unittest.TestCase):
     ''' class to test FileStorage clas,'''
+
     def test_type_of_obj(self):
+        """test type of instance if it is Filestorage"""
         stor = FileStorage()
         self.assertTrue(type(stor), FileStorage)
 
     def test_cls_attr_file_path_private(self):
+        """test whether the class attribute is set to private"""
         with self.assertRaises(AttributeError):
             FileStorage().__file_path
 
     def test_cls_attr_objects_private(self):
+        """test whether the class attribute is set to private"""
         with self.assertRaises(AttributeError):
             FileStorage.__objects
 
     def test_cls_method_all(self):
+        """test the return type is dict """
         self.assertEqual(dict, type(FileStorage().all()))
 
     def test_cls_attr_type(self):
+        """check type of private attr """
         self.assertEqual(str, type(FileStorage._FileStorage__file_path))
         self.assertEqual(dict, type(FileStorage._FileStorage__objects))
 
     def test_method_new(self):
+        """test whether the new obj is added """
         obj = BaseModel()
         stor = FileStorage()
         stor.new = obj
